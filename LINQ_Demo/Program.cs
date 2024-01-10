e6d0400fb9142384323ba1aee6bc48f40219411b
Priyanshu Kuldeep <itzchaturvedi@gmail.com>
2024-01-10 14:16:07 +0530

    LINQ in Collection

--- LINQ_Demo/Program.cs
+++ LINQ_Demo/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System;
+using System.Linq; // Namespace for LINQ
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Collection DataSource
+        int[] age = { 16, 18, 30, 23, 12, 23, 13, 14, };
+        //LINQ Query
+        var a = from i in age where i > 20 select i;
+
+        foreach (int item in a)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.ReadLine();
+    }
+}
